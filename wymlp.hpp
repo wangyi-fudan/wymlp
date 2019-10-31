219f74ac1a0ff5592471a4d4b80605050d6b665b
王一 WangYi <godspeed_china@yeah.net>
2019-10-31 22:10:53 +0800

    Add files via upload

--- wymlp.hpp
+++ wymlp.hpp
@@ -2,18 +2,13 @@
 #include	<math.h>
 template<class	type,	unsigned	input,	unsigned	hidden,	unsigned	depth,	unsigned	output,	unsigned	task>
 unsigned	wymlp(type	*weight,	type	*x,	type	*y,	type	eta,	uint64_t	seed,	double	dropout) {
-#ifdef	VanillaMLP
-	if(dropout<0)	return	(input+1)*hidden+(depth-1)*hidden*hidden+output*hidden;
-#define	woff(i,l)	(l?(input+1)*hidden+(l-1)*hidden*hidden+i*hidden:i*hidden)
-#else
-	if(dropout<0)	return	(input+1)*hidden+hidden*hidden+output*hidden;
-#define	woff(i,l)	(l?(l<depth?(input+1)*hidden+i*hidden:(input+1)*hidden+hidden*hidden+i*hidden ):i*hidden)
-#endif
-#define	act(x)	(x/(1+(((int)(x>0)<<1)-1)*x))
-#define	gra(x)	((1-(((int)(x>0)<<1)-1)*x)*(1-(((int)(x>0)<<1)-1)*x))
+	if(weight==NULL)	return	(input+1)*hidden+hidden*hidden+output*hidden;
+	#define	woff(i,l)	(l?(l<depth?(input+1)*hidden+i*hidden:(input+1)*hidden+hidden*hidden+i*hidden ):i*hidden)
+	#define	act(x)	(x/(1+(((int)(x>0)<<1)-1)*x))
+	#define	gra(x)	((1-(((int)(x>0)<<1)-1)*x)*(1-(((int)(x>0)<<1)-1)*x))
 	type	a[2*depth*hidden+output]= {},	*d=a+depth*hidden,	*o=a+2*depth*hidden,	wh=1/sqrtf(hidden),	wi=(1-(eta<0)*dropout)/sqrtf(input+1);	uint64_t	drop=dropout*~0ull;
-	for(unsigned  i=0;  i<=input; i++) if(eta<0||wyhash64(i,seed)>=drop) {
-		type	*w=weight+woff(i,0),	s=i==input?1:x[i];	if(s==0)	continue;
+	for(unsigned  i=0;  i<=input; i++)	{
+		type	*w=weight+woff(i,0),	s=(i==input?1:x[i])*(eta<0||wyhash64(i,seed)>=drop);
 		for(unsigned	j=0;	j<hidden;	j++)	a[j]+=s*w[j];
 	}
 	for(unsigned	i=0;	i<hidden;	i++) a[i]=i?act(wi*a[i]):1;
@@ -41,8 +36,8 @@ unsigned	wymlp(type	*weight,	type	*x,	type	*y,	type	eta,	uint64_t	seed,	double	d
 		}
 	}
 	for(unsigned	i=0;	i<hidden;	i++) d[i]*=gra(a[i])*wi;
-	for(unsigned  i=0;  i<=input; i++)	if(eta<0||wyhash64(i,seed)>=drop) {
-		type	*w=weight+woff(i,0),	s=(i==input?1:x[i]);	if(s==0)	continue;
+	for(unsigned  i=0;  i<=input; i++)	{
+		type	*w=weight+woff(i,0),	s=(i==input?1:x[i])*(eta<0||wyhash64(i,seed)>=drop);
 		for(unsigned	j=0;	j<hidden;	j++)	w[j]-=s*d[j];
 	}
 	return	0;
@@ -70,5 +65,8 @@ Comments:
 3: The expected |X[i]|, |Y[i]| should be around 1. Normalize yor input and output first.
 4: In practice, it is OK to call model function parallelly with multi-threads, however, they may be slower for small net.
 5: The code is portable, however, if Ofast is used on X86, SSE or AVX or even AVX512 will enable very fast code!
-6: The default and suggested model is shared hidden-hidden weights. If you want vanilla MLP, define VanillaMLP
+6: The default and suggested model is shared hidden-hidden weights. If you want vanilla MLP, use the following code
+if(weight==NULL)	return	(input+1)*hidden+(depth-1)*hidden*hidden+output*hidden;
+#define	woff(i,l)	(l?(input+1)*hidden+(l-1)*hidden*hidden+i*hidden:i*hidden)
+
 */
