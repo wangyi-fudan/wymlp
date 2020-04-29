78c2d3030ee05d73d65d171250990429ce61344a
王一 WangYi <godspeed_china@yeah.net>
2020-04-29 22:00:32 +0800

    Update wymlp_avx512f.hpp

--- wymlp_avx512f.hpp
+++ wymlp_avx512f.hpp
@@ -35,8 +35,6 @@ float	wymlp(float	*weight,	float	**x,	float	**y,	float	eta,	float	*a) {
 	}
 	float	ret=0;
 	for(unsigned	l=1;	l<=depth;	l++){
-	//	int	m=l==depth?output:hidden,	n=batch,	k=hidden,	lda=hidden,ldc=l==depth?output:hidden;	float	alpha=wh,beta=0;
-	//	SGEMM_("Y","N",&m,&n,&k,&alpha,woff(0,l),&lda,aoff(0,l-1),&lda,&beta,l==depth?o:aoff(0,l),&ldc);
 		cblas_sgemm('T','N',l==depth?output:hidden,batch,hidden,wh,woff(0,l),hidden,aoff(0,l-1),hidden,0,l==depth?o:aoff(0,l),l==depth?output:hidden);
 		for(unsigned    b=0;    b<batch;    b++){
 			if(l<depth){
@@ -62,10 +60,6 @@ float	wymlp(float	*weight,	float	**x,	float	**y,	float	eta,	float	*a) {
 		}
 		cblas_sgemm('N','N',hidden,batch,l==depth?output:hidden,1,woff(0,l),hidden,l==depth?o:doff(0,l),l==depth?output:hidden,0,doff(0,l-1),hidden);
 		cblas_sgemm('N','T',hidden,l==depth?output:hidden, batch,-1,aoff(0,l-1),hidden,l==depth?o:doff(0,l),l==depth?output:hidden,1,woff(0,l),hidden);
-//		int	m=hidden,	n=batch,	k=l==depth?output:hidden,	lda=hidden,ldb=l==depth?output:hidden, ldc=hidden;	float	alpha=1,beta=0;
-//		SGEMM_("N","N",&m,&n,&k,&alpha,woff(0,l),&lda,l==depth?o:doff(0,l),&ldb,&beta,doff(0,l-1),&ldc);
-//		m=hidden,	n=l==depth?output:hidden,	k=batch,	lda=hidden,ldb=l==depth?output:hidden, ldc=hidden;	alpha=-1,beta=1;
-//		SGEMM_("N","Y",&m,&n,&k,&alpha,aoff(0,l-1),&lda,l==depth?o:doff(0,l),&ldb,&beta,woff(0,l),&ldc);
 	}
 	for(unsigned    b=0;    b<batch;    b++){
 		w=woff(input,0);	p=aoff(b,0);	q=doff(b,0);
