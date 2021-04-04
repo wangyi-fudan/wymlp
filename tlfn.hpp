4a08d407e3b6d2d484e15783c23b8149d27a86b4
王一 WangYi <godspeed_china@yeah.net>
2021-04-04 03:06:26 +0000

    Add files via upload

--- tlfn.hpp
+++ tlfn.hpp
@@ -1,18 +1,20 @@
+#include	"wyhash.h"
 #include	<stdio.h>
 #include	<math.h>
-template<unsigned	input,	unsigned	hidden,	unsigned	output>
+template<uint32_t	input,	uint32_t	hidden,	uint32_t	output>
 class	tlfn{
 private:
 	float	acti(float	x) {	return  x/(1+(x>0?x:-x));	}
-	float	grad(float	x) {	return	x=1-(x>0?x:-x);	return	x*x;	}
+	float	grad(float	x) {	x=1-(x>0?x:-x);	return	x*x;	}
 public:
 	float	weight[(input+1)*hidden+hidden*hidden+output*hidden];
-	void	init(uint64_t	&seed){	for(unsigned	i=0;	i<sizeof(weight)/sizeof(float);	i++)	weight[i]=sqrtf(1+(i>=(input+1)*hidden))*wy2gau(wyrand(&seed));	}
-	
+
+	tlfn(uint64_t	seed){	for(uint32_t	i=0;	i<sizeof(weight)/sizeof(float);	i++)	weight[i]=wy2gau(wyrand(&seed));	}
+
 	bool	save(const	char	*F){
 		FILE	*f=fopen(F,	"wb");
 		if(f==NULL)	return	false;
-		unsigned	n;
+		uint32_t	n;
 		n=input;	fwrite(&n,4,1,f);
 		n=hidden;	fwrite(&n,4,1,f);
 		n=output;	fwrite(&n,4,1,f);
@@ -24,7 +26,7 @@ public:
 	bool	load(const	char	*F){
 		FILE	*f=fopen(F,	"rb");
 		if(f==NULL)	return	false;
-		unsigned	n;
+		uint32_t	n;
 		if(fread(&n,4,1,f)!=1||n!=input)	return	false;
 		if(fread(&n,4,1,f)!=1||n!=hidden)	return	false;
 		if(fread(&n,4,1,f)!=1||n!=output)	return	false;
@@ -32,52 +34,39 @@ public:
 		fclose(f);
 		return	true;
 	}
-	float	operator()(float	*x,	float	*y,	float	eta) {
-		float	a[4*hidden+output]={},	*a1=a+hidden,	*d=a1+hidden,	*d1=d+hidden,	*o=d1+hidden,	wh=1/sqrtf(hidden),	wi=1/sqrtf(input+1);
-		for(unsigned	i=0;	i<=input;	i++){
+
+	void	operator()(float	*x,	float	*y,	float	eta) {
+		float	a[4*hidden+output]={},	*a1=a+hidden,	*d=a1+hidden,	*d1=d+hidden,	*o=d1+hidden;
+		const	float	wh=1/sqrtf(hidden),	wi=1/sqrtf(input+1);
+		for(uint32_t	i=0;	i<=input;	i++){
 			float	s=i<input?x[i]:1,	*w=weight+i*hidden;
-			for(unsigned	j=0;	j<hidden;	j++)	a[j]+=s*w[j];
+			for(uint32_t	j=0;	j<hidden;	j++)	a[j]+=s*w[j];
 		}
-		for(unsigned	i=0;	i<hidden;	i++){	a[i]=acti(wi*a[i]);	}	a[0]=1;
-		for(unsigned	i=0;	i<hidden;	i+=4){
-			float	s0=0,	*w0=weight+(input+1)*hidden+i*hidden;
-			float	s1=0,	*w1=w0+hidden;
-			float	s2=0,	*w2=w1+hidden;
-			float	s3=0,	*w3=w2+hidden;
-			for(unsigned	j=0;	j<hidden;	j++){	s0+=a[j]*w0[j];	s1+=a[j]*w1[j];	s2+=a[j]*w2[j];	s3+=a[j]*w3[j];	}
-			a1[i]=s0;	a1[i+1]=s1;	a1[i+2]=s2;	a1[i+3]=s3;
+		for(uint32_t	i=0;	i<hidden;	i++){	a[i]=acti(wi*a[i]);	}	a[0]=1;
+		for(uint32_t	i=0;	i<hidden;	i+=4){
+			float	s=0,	*w=weight+(input+1)*hidden+i*hidden;
+			for(uint32_t	j=0;	j<hidden;	j++)	s+=a[j]*w[j];
+			a1[i]=s;
 		}
-		for(unsigned	i=0;	i<hidden;	i++){	a1[i]=acti(a1[i]*wh);	}	a1[0]=1;
-		for(unsigned	i=0;	i<output;	i++){
+		for(uint32_t	i=0;	i<hidden;	i++){	a1[i]=acti(a1[i]*wh);	}	a1[0]=1;
+		for(uint32_t	i=0;	i<output;	i++){
 			float	s=0,	*w=weight+(input+1)*hidden+hidden*hidden+i*hidden;
-			for(unsigned	j=0;	j<hidden;	j++)	s+=w[j]*a1[j];
+			for(uint32_t	j=0;	j<hidden;	j++)	s+=w[j]*a1[j];
 			o[i]=s*wh;
 		}
-		if(eta<0){	for(unsigned	i=0;	i<output;	i++){	y[i]=o[i];	}	return	0;	}
-		float	loss=0;
-		for(unsigned	i=0;	i<output;	i++){
-			loss+=fabsf(o[i]-y[i]);
-			float	s=(o[i]>y[i]?1:-1)*wh*eta,	*w=weight+(input+1)*hidden+hidden*hidden+i*hidden;
-			for(unsigned	j=0;	j<hidden;	j++){	d1[j]+=s*w[j];	w[j]-=s*a1[j];	}
+		if(eta<0){	for(uint32_t	i=0;	i<output;	i++){	y[i]=o[i];	}	return;	}
+		for(uint32_t	i=0;	i<output;	i++){
+			float	s=fminf(fmaxf(o[i]-y[i],-1),1)*wh*eta,	*w=weight+(input+1)*hidden+hidden*hidden+i*hidden;
+			for(uint32_t	j=0;	j<hidden;	j++){	d1[j]+=s*w[j];	w[j]-=s*a1[j];	}
+		}	d1[0]=0;
+		for(uint32_t	i=0;	i<hidden;	i++){
+			float	s=d1[i]*grad(a1[i])*wh,	*w=weight+(input+1)*hidden+i*hidden;
+			for(uint32_t	j=0;	j<hidden;	j++){	d[j]+=s*w[j];	w[j]-=s*a[j];	}
 		}
-		d1[0]=0;
-		for(unsigned	i=0;	i<hidden;	i+=4){
-			float	s0=d1[i]*grad(a1[i])*wh,	*w0=weight+(input+1)*hidden+i*hidden;
-			float	s1=d1[i+1]*grad(a1[i+1])*wh,	*w1=w0+hidden;
-			float	s2=d1[i+2]*grad(a1[i+2])*wh,	*w2=w1+hidden;
-			float	s3=d1[i+3]*grad(a1[i+3])*wh,	*w3=w2+hidden;
-			#pragma GCC ivdep
-			for(unsigned	j=0;	j<hidden;	j++){
-				d[j]+=s0*w0[j]+s1*w1[j]+s2*w2[j]+s3*w3[j];
-				w0[j]-=s0*a[j];	w1[j]-=s1*a[j];	w2[j]-=s2*a[j];	w3[j]-=s3*a[j];	
-			}
-		}
-		for(unsigned	i=0;	i<hidden;	i++)	d[i]*=grad(a[i])*wi;
-		d[0]=0;
-		for(unsigned	i=0;	i<=input;	i++){
+		for(uint32_t	i=0;	i<hidden;	i++){	d[i]*=grad(a[i])*wi;	}	d[0]=0;
+		for(uint32_t	i=0;	i<=input;	i++){
 			float	s=i<input?x[i]:1,	*w=weight+i*hidden;
-			for(unsigned	j=0;	j<hidden;	j++)	w[j]-=s*d[j];
+			for(uint32_t	j=0;	j<hidden;	j++)	w[j]-=s*d[j];
 		}
-		return	loss;
 	}
 };
